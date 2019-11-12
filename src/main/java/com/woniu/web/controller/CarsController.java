aa3d2ef95905f406479deb7aa0ab45eb6f93bd0f
gzh <Administrator@192.168.9.209>
2019-11-12 20:57:00 +0800

    master 合并分支

--- src/main/java/com/woniu/web/controller/CarsController.java
+++ src/main/java/com/woniu/web/controller/CarsController.java
@@ -3,30 +3,23 @@ package com.woniu.web.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.woniu.pojo.Cars;
 import com.woniu.pojo.CarsExample;
-import com.woniu.service.ICarsService;
 import com.woniu.service.impl.CarsServiceImpl;
 
 
